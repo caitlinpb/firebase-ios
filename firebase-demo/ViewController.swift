9be5de2b9286278363882db6e495d06684931561
caitlinpb <caitlinpalmerbright@gmail.com>
2019-03-15 10:55:53 +1000

    Initial Commit

---
+++ firebase-demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  firebase-demo
+//
+//  Created by Caitlin Palmer-Bright on 15/3/19.
+//  Copyright © 2019 Bitsmith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
